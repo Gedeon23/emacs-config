30b14bf8229638793cfc1fe8862055dcc8eefed5
Gedeon Jahns <gedeon.jahns@gmail.com>
2023-05-01 00:50:27 +0200

    MAGIT

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
+ '(package-selected-packages
+   '(evil-magit magit counsel-projectile projectile hydra evil-collection which-key use-package rainbow-delimiters ivy-rich helpful general evil doom-themes doom-modeline counsel command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
